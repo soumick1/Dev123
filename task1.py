ad386d0a74c2d53d987cbc8322986e43b7ecfe06
soumick1 <64783562+soumick1@users.noreply.github.com>
2020-05-04 18:05:12 +0530

    tasks 1&2
    1)prints area of a circle
    2)prints extension of a file

---
+++ task1.py
@@ -0,0 +1,4 @@
+import math
+r=float(input("Input the radius of the circle: "))
+area=math.pi*r*r
+print("The area of the circle with radius ",r," is: ",area)
---
+++ task2.py
@@ -0,0 +1,3 @@
+file_name=input("Input the Filename: ")
+extn=file_name.split(".")
+print("The extension of the file is: "+extn[-1])
